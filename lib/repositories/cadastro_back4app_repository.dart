22c9d5d71710fb2436ea3e1617e39f77a8455b7d
Charles Xavier <charlesxsaldanha@hotmail.com>
2023-10-01 17:43:23 -0300

    feat: criado arquivo repository para back4app, com get para listagem dos endereços

--- lib/repositories/cadastro_back4app_repository.dart
+++ lib/repositories/cadastro_back4app_repository.dart
@@ -4,6 +4,11 @@ import 'package:dio/dio.dart';
 class CadastroBack4AppRepository {
   Future<CadastroBack4AppCEPModel> obterCadastros() async {
     var dio = Dio();
+    dio.options.headers['X-Parse-Application-Id'] =
+        'ZW5MnH7abyPHDOLX7RubnT2pSGzm0v4oYCVKRGMh';
+    dio.options.headers['X-Parse-REST-API-Key'] =
+        '2tQQxDwA2LIfDMqNcws5DnoLzNBmUafQOyyd0R1g';
+    dio.options.headers['content-type'] = 'application/json';
     var result = await dio.get('https://parseapi.back4app.com/classes/CEP');
     return CadastroBack4AppCEPModel.fromJson(result.data);
   }
