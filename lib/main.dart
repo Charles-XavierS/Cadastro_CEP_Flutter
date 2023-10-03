e95a3c541bf205523e4b823d862e32d5c5aa508f
Charles Xavier <charlesxsaldanha@hotmail.com>
2023-10-03 19:42:41 -0300

    feat: app fazendo busca de cep e podendo salvar no back4app se não estiver salvo

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:cadastro_cep/model/cadastro_back4app_model.dart';
 import 'package:cadastro_cep/repositories/cadastro_back4app_repository.dart';
 import 'package:flutter/material.dart';
 
+import 'model/viacep_model.dart';
+import 'repositories/viacepe_api_repository.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +18,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
-        // useMaterial3: true, // Remova essa linha, pois não é mais necessária no Flutter 2.0+
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -34,6 +36,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   CadastroBack4AppRepository cadastroRepository = CadastroBack4AppRepository();
   var _cadastros = CadastroBack4AppCEPModel([]);
+  final TextEditingController _cepController = TextEditingController();
+  String _mensagem = "";
 
   @override
   void initState() {
@@ -44,6 +48,96 @@ class _MyHomePageState extends State<MyHomePage> {
   void obterCadastros() async {
     _cadastros = await cadastroRepository.obterCadastros();
     setState(() {});
+  }
+
+  Future<void> _buscarEndereco() async {
+    final cep = _cepController.text;
+    if (cep.isEmpty) {
+      setState(() {
+        _mensagem = "Digite um CEP válido.";
+      });
+      return;
+    }
+
+    try {
+      final endereco = await ViaCEPRepository().consultarCep(cep);
+      final enderecoExiste =
+          await cadastroRepository.enderecoExisteNoBack4App(cep);
+
+      if (enderecoExiste) {
+        setState(() {
+          _mensagem = "Este endereço já está cadastrado no Back4App.";
+        });
+      } else {
+        final result = await _mostrarDialogoSalvarEndereco(endereco);
+
+        if (result == true) {
+        } else {
+          setState(() {
+            _mensagem = "Endereço não foi salvo.";
+          });
+        }
+      }
+    } catch (e) {
+      // Trate erros aqui
+      setState(() {
+        _mensagem = "Erro ao buscar o endereço.";
+      });
+    }
+  }
+
+  Future<bool?> _mostrarDialogoSalvarEndereco(ViaCEPModel endereco) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Salvar Endereço'),
+        content: const Text('Deseja salvar este endereço no Back4App?'),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await fazerCadastro(endereco);
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Sim'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Não'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> fazerCadastro(ViaCEPModel endereco) async {
+    String? cepString = endereco.cep.replaceAll("-", "");
+    int? cepInt = int.tryParse(cepString);
+    int? numero = int.tryParse(endereco.complemento);
+
+    var novo = await cadastroRepository.fazerCadastro(Cadastros.criar(
+      cep: cepInt ?? 0,
+      logradouro: endereco.logradouro,
+      numero: numero ?? 0,
+      complemento: endereco.complemento,
+      bairro: endereco.bairro,
+      localidade: endereco.localidade,
+      estado: endereco.uf,
+    ));
+
+    try {
+      setState(() {
+        novo;
+        _mensagem = "Endereço salvo com sucesso!";
+        _cepController.clear();
+        obterCadastros();
+      });
+    } catch (e) {
+      setState(() {
+        _mensagem = "Erro ao salvar o endereço.";
+      });
+    }
   }
 
   @override
@@ -68,7 +162,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       children: [
                         Text(resultados.cep.toString()),
                         Text(resultados.logradouro),
-                        Text(resultados.complemento),
+                        Text(resultados.numero.toString()),
                         Text(resultados.bairro),
                         Text(resultados.localidade),
                         Text(resultados.estado),
@@ -78,6 +172,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _cepController,
+                decoration: const InputDecoration(labelText: 'Digite o CEP'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => _buscarEndereco(),
+              child: const Text('Buscar Endereço'),
+            ),
+            Text(_mensagem),
           ],
         ),
       ),
