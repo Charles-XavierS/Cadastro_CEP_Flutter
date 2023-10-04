3cf58de5137681474bcea2a49ee90466f30c1889
Charles Xavier <charlesxsaldanha@hotmail.com>
2023-10-04 16:26:34 -0300

    feat: Criado widget CustomTextField

---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final bool obscureText;
+  final bool enabled;
+  final bool focusNode;
+  final TextInputType keyboardType;
+  final int? maxLength;
+  final void Function(String)? onChanged;
+  final FormFieldValidator<String>? validator;
+
+  const CustomTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.controller,
+    this.obscureText = false,
+    this.enabled = true,
+    this.focusNode = false,
+    this.keyboardType = TextInputType.text,
+    this.maxLength,
+    this.onChanged,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  final FocusNode _focusNode = FocusNode();
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      _isFocused = _focusNode.hasFocus;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      child: TextFormField(
+        controller: widget.controller,
+        enabled: widget.enabled,
+        obscureText: widget.obscureText,
+        keyboardType: widget.keyboardType,
+        maxLength: widget.maxLength,
+        onChanged: (value) {
+          if (widget.onChanged != null) {
+            widget.onChanged!(value);
+          }
+        },
+        focusNode: _focusNode,
+        cursorColor: Colors.blue,
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          counterText: '',
+          labelStyle: TextStyle(
+            color: _isFocused ? Colors.blue : Colors.grey,
+          ),
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+        ),
+        validator: widget.validator,
+      ),
+    );
+  }
+}
